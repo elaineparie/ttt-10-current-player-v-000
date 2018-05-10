4854b6fe7240dfef62e15d309599378110df87e3
Elaine Anderson <eparie@bu.edu>
2018-05-10 16:36:50 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,11 +11,11 @@ counter
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
+  if turn_count(board) % 2 = 0
     puts "X"
-  elsif turn_count(board) % 2 == 1 
+  elsif turn_count(board) % 2 = 1
     puts "O"
-  else 
+  else
     nil
 end
 end
