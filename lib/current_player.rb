8f804dbf9fb2beb7c70668397d2660a29ced13ae
Elaine Anderson <eparie@bu.edu>
2018-05-10 15:45:08 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@
 def turn_count(board)
   board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
   counter = 0
-turn_count(board).each do |turn|
+turn_count.each do |turn|
   if board != "   "
   counter += 1
 else
